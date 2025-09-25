e0bd191a967adfe7d3ecc871b6e4f5345c736233
anantayot <67010974018@msu.ac.th>
2025-09-25 16:07:54 +0700

    a

--- delete_product.php
+++ delete_product.php
@@ -3,7 +3,7 @@ include "connectdb.php";
 
 if (isset($_GET['delete_id'])) {
     $id = $_GET['delete_id'];
-    $sql = "DELETE FROM category WHERE cat_id=$id";
+    $sql = "DELETE FROM product WHERE p_id=$id";
     mysqli_query($conn, $sql);
 }
 header("Location: index.php");
