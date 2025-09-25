a78f8c049ac60d3e16a37244917293612ec89b3f
anantayot <67010974018@msu.ac.th>
2025-09-25 15:32:53 +0700

    ฟ

---
+++ connectdb.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pwd = "note071245";
+	$dbname = "snekker_shop";
+	$conn = mysqli_connect($host,$user,$pwd,$dbname);
+	mysqli_query($conn, "SET NAMES utf8");
+	
+?>
