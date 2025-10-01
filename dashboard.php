46c0bd0ccd0185ab1f618a253b8476d4e2c81866
anantayot <67010974018@msu.ac.th>
2025-10-01 18:37:11 +0700

    a

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php include 'admin_header.php'; ?>
+<h1>แดชบอร์ด</h1>
+<div class="row">
+<div class="col-md-3">
+<div class="card p-3 mb-3"><h5>สินค้า</h5><a href="products.php" class="stretched-link"></a></div>
+</div>
+<div class="col-md-3">
+<div class="card p-3 mb-3"><h5>ประเภท</h5><a href="categories.php" class="stretched-link"></a></div>
+</div>
+<div class="col-md-3">
+<div class="card p-3 mb-3"><h5>ลูกค้า</h5><a href="customers.php" class="stretched-link"></a></div>
+</div>
+<div class="col-md-3">
+<div class="card p-3 mb-3"><h5>ออเดอร์</h5><a href="orders.php" class="stretched-link"></a></div>
+</div>
+</div>
+<?php include 'admin_footer.php'; ?>
