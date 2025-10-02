34d693b9d7ce73591dd1c39f3664c08467c54fdb
anantayot <67010974018@msu.ac.th>
2025-10-02 17:45:28 +0700

    a

---
+++ user/connectdb.php
@@ -0,0 +1,11 @@
+
+<?php
+$servername = "localhost";
+$dbuser = "root";
+$dbpass = "note071245";
+$dbname = "ihavejib";
+$conn = new mysqli($servername, $dbuser, $dbpass, $dbname);
+if ($conn->connect_error) {
+    die("DB connect error: " . $conn->connect_error);
+}
+$conn->set_charset("utf8mb4");
